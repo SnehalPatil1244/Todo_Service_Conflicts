a50b12df89a192a99156b7c2c206bfd3f25b4f03
Snehal Patil. <patilsnehal1469@gmail.com>
2026-05-20 18:04:40 +0530

    github

---
+++ src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { TodoDashboardComponent } from './shared/component/todo-dashboard/todo-dashboard.component';
+import { TodoFormComponent } from './shared/component/todo-form/todo-form.component';
+import { TodoListComponent } from './shared/component/todo-list/todo-list.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    TodoDashboardComponent,
+    TodoFormComponent,
+    TodoListComponent
+  ],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
